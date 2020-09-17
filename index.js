227edc5699a542b8f89d4f67de0f15a113de0902
kevinneyer <kevinneyer@gmail.com>
2020-09-17 12:50:08 -0400

    first function complete

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function findMinAndRemove(array){
-
+    let min = array[0]
+    let index;
+    for(let i = 0; i < array.length; i++){
+        if(array[i] < min){
+            min = array[i]
+            index = i
+        } 
+    }
+    array.splice(index, 1)
+    return min
 }
 
 function selectionSort(array){
